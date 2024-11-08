72e1ec6f8bdff7d6735d28cb6b43185b07ce9f5d
Davilapeixoto <davilapeixoto52@gmail.com>
2024-11-08 11:01:53 -0300

    Update trabalho.py
    criei uma forma de pesquisa, criei uma biblioteca para nome, tempo e data e assim o usuário consegue pesquisar por nome tempo ou data dependendo da escolha, ainda é necessário prevenir erros e fazer o mesmo para o update, alem de criar um txt para receber todos os nomes, tempo e datas para ficar salvo caso a execução pare

--- trabalho.py
+++ trabalho.py
@@ -1,5 +1,8 @@
 import os
 os.system("cls")
+arquivos_nome={}
+arquivos_data={}
+arquivos_tempo={}
 while True:
     print("1-Criar um treino\n2-visualizar treinos\n3-atualizar treinos\n4-deletar\n5-limpar terminal\n6-Sair")
     try:
@@ -7,20 +10,38 @@ while True:
     except NameError:
         print("Tente um numero")
     if esc==1:
-        arquivo=open("trabalho/treinos.txt","a")
-        treino=input("Como foi o treino: ")
-        data=input("Qual a data: ")
-        distancia=input("Qual a distancia: ")
-        tempo=input("Qual foi o tempo de corrida")
-        localizacao=input("Qual foi a localização: ")
-        clima=input("Quais foram as condições climaticas")
-        arquivo.write(f"Data: {data}\nDistancia: {distancia}\nTempo: {tempo}\nLocalização: {localizacao}\nClima: {clima}\nTreino: {treino}\n")
-        arquivo.close()
+        nome=input("Qual o nome do novo arquivo")
+        if os.path.exists(f"trabalho/{nome}.txt"):
+            print("Use outro nome")
+        else:
+            arquivo=open(f"trabalho/{nome}.txt","a")
+            treino=input("Como foi o treino: ")
+            data=input("Qual a data: ")
+            distancia=input("Qual a distancia: ")
+            tempo=input("Qual foi o tempo de corrida")
+            localizacao=input("Qual foi a localização: ")
+            clima=input("Quais foram as condições climaticas")
+            arquivo.write(f"Data: {data}\nDistancia: {distancia}\nTempo: {tempo}\nLocalização: {localizacao}\nClima: {clima}\nTreino: {treino}\n")
+            caminho=f"trabalho/{nome}.txt"
+            arquivos_nome[nome]=caminho
+            arquivos_data[data]=caminho
+            arquivos_tempo[tempo]=caminho
+            arquivo.close()
 
     elif esc==2:
-        leitura=open("trabalho/treinos.txt","r")
-        print(leitura.read())
-        leitura.close()
+        escolha=input("Voce deseja pesquisar por Nome/Data/Tempo: ").lower().strip()
+        nome=input("Qual o treino")
+        if escolha=="nome" and nome in arquivos_nome:
+            caminho=arquivos_nome[nome]
+        elif escolha=="data" and nome in arquivos_data:
+            caminho=arquivos_data[data]
+        elif escolha=="tempo" and nome in arquivos_tempo:
+            caminho=arquivos_tempo[tempo]
+        else:
+            print("Arquivo Inexistente")
+            continue
+        with open(caminho,"r") as ler:
+            print(ler.read())
     elif esc==3:
         arquivo=open("trabalho/treinos.txt","w")
         treino=input("Como foi o treino: ")
