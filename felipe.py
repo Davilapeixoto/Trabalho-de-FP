8cbad0a39f9eaa2e00612bf336df382dcd0135c2
Davilapeixoto <davilapeixoto52@gmail.com>
2024-11-20 18:24:13 -0300

    Update felipe.py

--- felipe.py
+++ felipe.py
@@ -84,6 +84,103 @@ def gerar_grafico():
         plt.show()
     else:
         print("Não há dados suficientes para gerar o gráfico.")
+def arquivo_metas():
+    try:
+        if os.path.exists("metas.txt"):
+            with open("metas.txt","r") as file:
+                metas = file.readlines()
+                metas = [meta.strip().split(",") for meta in metas]
+                metas = [[descricao, int(valor)] for descricao, valor in metas]
+        else:
+            metas = []
+        return metas
+    except Exception as e:
+        print(f"Erro ao carregar o arquivo de metas: {e}")
+        return []
+def salvar_metas(metas):
+    try:
+        with open("metas.txt","w") as file:
+            for descricao, valor in metas:
+                file.write(f"{descricao},{valor}\n")
+    except Exception as e:
+        print(f"Erro ao salvar as metas no arquivo: {e}")
+def adicionar_meta(metas):
+    try:
+        descricao = input("Digite a descrição da meta (exemplo: Correr 100 km por mês ou melhorar o tempo em 5 km):")
+        valor_meta = input("Digite o valor da meta (exemplo: 100 para 100 km): ")
+        if valor_meta.isdigit():
+            metas.append([descricao, int(valor_meta)])
+            print("Meta adicionada com sucesso.")
+            salvar_metas(metas)
+        else:
+            print("Insira um valor numérico válido para a meta")
+    except Exception as e:
+        print(f"Erro ao adicionar a meta: {e}")
+def atualizar_meta(metas):
+    try:
+        if not metas:
+            print("Não há metas para atualizar.")
+            return
+        mostrar_metas(metas)
+        meta_id = input("Escolha o número da meta que deseja atualizar: ")
+        if meta_id.isdigit() and 1 <= int(meta_id) <= len(metas):
+            meta_id = int(meta_id)
+            alterar_descricao = input("Você gostaria de alterar a descrição da meta? (s/n) ou sim/não): ")
+            if alterar_descricao.lower() == 's' or alterar_descricao.lower() == 'sim':
+                nova_descricao = input("Digite a nova descrição da meta: ")
+                metas[meta_id - 1][0] = nova_descricao
+                print(f"Descrição da meta atualizada para: {nova_descricao}")
+            novo_valor = input("Digite o novo valor para a meta: ")
+            if novo_valor.isdigit():
+                metas[meta_id - 1][1] = int(novo_valor) 
+                print(f"Meta atualizada com sucesso para {novo_valor}.")
+                salvar_metas(metas)
+            else:
+                print("Insira um valor numérico válido para a nova meta.")
+        else:
+            print("Número de meta inválido.")
+    except Exception as e:
+        print(f"Erro ao atualizar a meta: {e}")
+def mostrar_metas(metas):
+    try:
+        if not metas:
+            print("Nenhuma meta definida.")
+        else:
+            print("\nMeta(s) Atual(is):")
+            for idx, (descricao, valor) in enumerate(metas, start=1):
+                print(f"{idx}. {descricao} - {valor}")
+    except Exception as e:
+        print(f"Erro ao exibir as metas: {e}")
+def registrar_progresso(metas):
+    try:
+        if not metas:
+            print("Não há metas para registrar o progresso em.")
+            return
+        mostrar_metas(metas)
+        meta_id = (input("Escolha o número da meta para registrar o progresso em: "))
+        if meta_id.isdigit() and 1 <= int(meta_id) <= len(metas):
+            meta_id = int(meta_id) 
+            progresso = input("Digite o progresso(exemplo: Corri 10 km): ")
+            if progresso.isdigit():
+                progresso = int(progresso)
+                descricao, valor_meta = metas[meta_id - 1]
+                valor_meta -= progresso
+                if valor_meta < 0:
+                    valor_meta = 0
+                metas[meta_id - 1] [1] = valor_meta
+                print(f"Progresso registrado: {progresso} para a meta de {metas[meta_id - 1][0]} com valor {metas[meta_id - 1][1]}.")
+                salvar_metas(metas)
+                if valor_meta == 0:
+                    print(f"a sua meta {descricao} foi atingida.")
+            else:
+                print("Insira um valor numérico válido para o progresso.")
+        else:
+            print("Número de meta inválido.")
+    except ValueError:
+        print("insira um número válido.")
+    except Exception as e:
+        print(f"Erro ao registrar o progresso: {e}")
+
 arquivos_treino=carregar()
 
 
@@ -179,10 +276,34 @@ while True:
             print("Treino não encontrado no histórico ou inexistente.")
     
     elif esc==5:
-        velocidade = int(0)
-        tempo = int(0)
-        distancia = int(0)
-        tipo_meta = input("Digite o tipo de meta que você quer implementar: ")
+        metas = arquivo_metas()
+        while True:
+            print("\nSistema de Gerenciamento de Metas")
+            print("1.Adicionar uma meta")
+            print("2.Mostrar metas")
+            print("3.Registrar progresso de uma meta")
+            print("4.Atualizar meta")
+            print("5.Sair")
+
+            try:
+                opcao = int(input("Escolha uma opção "))
+                if opcao == 1:
+                    adicionar_meta(metas)
+                elif opcao == 2:
+                    mostrar_metas(metas)
+                elif opcao == 3:
+                    registrar_progresso(metas)
+                elif opcao == 4:
+                    atualizar_meta(metas)
+                elif opcao == 5:
+                    print("Saindo do sistema.")
+                    break
+                else:
+                    print("Opção inválida.Tente uma opção de 1 á 5")
+            except ValueError:
+                print("Insira uma opção válida.")
+            except Exception as e:
+                print(f"Erro inesperado: {e}")
     
     elif esc==6:
         print("Treino aleatorio: ")
